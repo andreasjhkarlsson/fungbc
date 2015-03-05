206a71e57b8679c0d00b398d257a41f86d4f5970
Andreas Karlsson <andreas.karlsson3@gmail.com>
2015-03-05 22:14:03 +0100

    Made function more beautiful

--- BitLogic.fs
+++ BitLogic.fs
@@ -2,9 +2,9 @@
 
 type BitState = |SET |CLEAR
 
-let bitStateToValue state = match state with | SET -> 1uy | CLEAR -> 0uy
+let bitStateToValue = function |SET -> 1uy |CLEAR -> 0uy
 
-let bitStateOf bit value  = if ((value >>> bit) &&& 1uy) = 1uy then SET else CLEAR
+let bitStateOf bit value = if ((value >>> bit) &&& 1uy) = 1uy then SET else CLEAR
 
 let bitStateInvert = function |SET -> CLEAR |CLEAR -> SET
 

d952732132667c0a7625eb09266fe63ecc0341dc
Andreas Karlsson <andreas.karlsson3@gmail.com>
2015-03-24 09:16:00 +0100

    Simplify PC manipulation in light of new knowledge

--- Cpu.fs
+++ Cpu.fs
@@ -131,7 +131,7 @@ type CPU (mmu, timerInterrupt: TimerInterrupt, clock: MutableClock) as this =
 
         let instructionSize = sizeOf instruction
 
-        let nextInstruction = PC.Value + (uint16 instructionSize)
+        PC.Value <- PC.Value + (uint16 instructionSize)
 
         // Just one little mutable flag. Sorry purists.
         let mutable longCycle = false
@@ -149,181 +149,126 @@ type CPU (mmu, timerInterrupt: TimerInterrupt, clock: MutableClock) as this =
         *)
         | LD_R8_R8 (r1,r2) ->
             (r8 r1).Value <- (r8 r2).Value
-            PC.Value <- nextInstruction
         | LD_R8_D8 (r,d) ->
             (r8 r).Value <- d
-            PC.Value <- nextInstruction
         | LD_A16_R8 (a,r) ->
             mmu.Write8 a ((r8 r).Value)
-            PC.Value <- nextInstruction
         | LD_R8_A16 (r,a) ->
             (r8 r).Value <- mmu.Read8 a
-            PC.Value <- nextInstruction
         | LD_R8_AR16 (r, ar) ->
             (r8 r).Value <- mmu.Read8 (r16 ar).Value
-            PC.Value <- nextInstruction
         | LD_AR16_R8 (ar, r) ->
             mmu.Write8 (r16 ar).Value (r8 r).Value
-            PC.Value <- nextInstruction
         | LD_R16_D16 (r,value) ->
             (r16 r).Value <- value
-            PC.Value <- nextInstruction
         | LDI_R8_AR16 (r,ar) ->
             (r8 r).Value <- mmu.Read8 (r16 ar).Value
             (r16 ar).Update ((+) 1us)
-            PC.Value <- nextInstruction
         | LDI_AR16_R8 (ar,r) ->
             mmu.Write8 (r16 ar).Value (r8 r).Value
             (r16 ar).Update ((+) 1us)
-            PC.Value <- nextInstruction
         | LDD_R8_AR16 (r,ar) ->
             (r8 r).Value <- mmu.Read8 (r16 ar).Value
             (r16 ar).Value <- (r16 ar).Value - 1us
-            PC.Value <- nextInstruction
         | LDD_AR16_R8 (ar,r) ->
             mmu.Write8 (r16 ar).Value (r8 r).Value
             (r16 ar).Value <- (r16 ar).Value - 1us
-            PC.Value <- nextInstruction
         | LDH_A8_R8 (offset, r) ->
             mmu.Write8 (0xFF00us + (uint16 offset)) (r8 r).Value
-            PC.Value <- nextInstruction
         | LDH_R8_A8 (r, offset) ->
             (r8 r).Value <- mmu.Read8 (0xFF00us + (uint16 offset))
-            PC.Value <- nextInstruction
         | LDH_AR8_R8 (ar, r) ->
             mmu.Write8 (0xFF00us + (uint16 (r8 ar).Value)) (r8 r).Value
-            PC.Value <- nextInstruction
         | PUSH_R16 (r) ->
             push16 (r16 r).Value
-            PC.Value <- nextInstruction
         | POP_R16 (r) ->
             (r16 r).Value <- pop16 ()
-            PC.Value <- nextInstruction
         (*
             ALU operations
         *)
         | ADD_R8_R8 (r1,r2) ->
             (r8 r1).Value <- alu.Add8 (r8 r1).Value (r8 r2).Value 0uy
-            PC.Value <- nextInstruction
         | ADD_R8_D8 (r,operand) ->
             (r8 r).Value <- alu.Add8 (r8 r).Value operand 0uy
-            PC.Value <- nextInstruction
         | ADD_R8_AR16 (r,ar) ->
             (r8 r).Value <- alu.Add8 (r8 r).Value (mmu.Read8 (r16 ar).Value) 0uy
-            PC.Value <- nextInstruction
         | ADD_R16_R16 (r1,r2) ->
             (r16 r1).Value <- alu.Add16 (r16 r1).Value (r16 r2).Value
-            PC.Value <- nextInstruction
         | ADC_R8_R8 (r1, r2) ->
             (r8 r1).Value <- alu.Add8 (r8 r1).Value (r8 r2).Value (bitStateToValue F.C)
-            PC.Value <- nextInstruction
         | ADC_R8_D8 (r, operand) ->
             (r8 r).Value <- alu.Add8 (r8 r).Value operand (bitStateToValue F.C)
-            PC.Value <- nextInstruction
         | ADC_R8_AR16 (r, ar) ->
             (r8 r).Value <- alu.Add8 (r8 r).Value (mmu.Read8 (r16 ar).Value) (bitStateToValue F.C)
-            PC.Value <- nextInstruction
         | SUB_R8_R8 (r1,r2) ->
             (r8 r1).Value <- alu.Sub8 (r8 r1).Value (r8 r2).Value 0uy
-            PC.Value <- nextInstruction
         | SUB_R8_AR16 (r,ar) ->
             (r8 r).Value <- alu.Sub8 (r8 r).Value (mmu.Read8 (r16 ar).Value) 0uy
-            PC.Value <- nextInstruction
         | SUB_R8_D8 (r, operand) ->
             (r8 r).Value <- alu.Sub8 (r8 r).Value operand 0uy
-            PC.Value <- nextInstruction
         | SBC_R8_R8 (r1, r2) ->
             (r8 r1).Value <- alu.Sub8 (r8 r1).Value (r8 r2).Value (bitStateToValue F.C)
-            PC.Value <- nextInstruction
         | SBC_R8_AR16 (r, ar) ->
             (r8 r).Value <- alu.Sub8 (r8 r).Value (mmu.Read8 (r16 ar).Value) (bitStateToValue F.C)
-            PC.Value <- nextInstruction
         | SBC_R8_D8 (r, operand) ->
             (r8 r).Value <- alu.Sub8 (r8 r).Value operand (bitStateToValue F.C)
-            PC.Value <- nextInstruction
         | CP_R8_R8  (r1,r2) ->
             alu.Compare8 (r8 r1).Value (r8 r2).Value
-            PC.Value <- nextInstruction
         | CP_R8_AR16 (r, ar) ->
             alu.Compare8 (r8 r).Value (mmu.Read8 (r16 ar).Value)
-            PC.Value <- nextInstruction
         | CP_R8_D8 (r, operand) ->
             alu.Compare8 (r8 r).Value operand
-            PC.Value <- nextInstruction
         | INC_R16 (r) ->
             (r16 r).Update alu.Inc16
-            PC.Value <- nextInstruction
         | DEC_R16 (r) ->
             (r16 r).Update alu.Dec16
-            PC.Value <- nextInstruction
         | INC_R8 (r) ->
             (r8 r).Update alu.Inc8
-            PC.Value <- nextInstruction
         | DEC_R8 (r) ->
             (r8 r).Update alu.Dec8
-            PC.Value <- nextInstruction
         | SWAP_R8 (r) ->
             (r8 r).Update alu.SwapNibbles
-            PC.Value <- nextInstruction
         | SWAP_AR16 (r) ->
             mmu.Update8 (r16 r).Value alu.SwapNibbles
-            PC.Value <- nextInstruction
         | CPL ->
             A.Update alu.BitNot8
-            PC.Value <- nextInstruction
         | RLC_R8 (r) ->
             (r8 r).Update alu.RotateLeftWithCarry8
-            PC.Value <- nextInstruction
         | AND_R8_R8 (r1,r2) ->
             (r8 r1).Update (alu.And8 (r8 r2).Value)
-            PC.Value <- nextInstruction
         | AND_R8_D8 (r,operand) ->
             (r8 r).Update (alu.And8 operand)
-            PC.Value <- nextInstruction
         | AND_R8_AR16 (r,ar) ->
             (r8 r).Update (alu.And8 <| mmu.Read8 (r16 ar).Value)
-            PC.Value <- nextInstruction
         | OR_R8_R8 (r1,r2) ->
             (r8 r1).Update (alu.Or8 (r8 r2).Value)
-            PC.Value <- nextInstruction
         | OR_R8_D8 (r,operand) ->
             (r8 r).Update (alu.Or8 operand)
-            PC.Value <- nextInstruction
         | OR_R8_AR16 (r,ar) ->
             (r8 r).Update (alu.Or8 <| mmu.Read8 (r16 ar).Value)
-            PC.Value <- nextInstruction
         | XOR_R8_R8 (r1,r2) ->
             (r8 r1).Update (alu.Xor8 (r8 r2).Value)
-            PC.Value <- nextInstruction
         | XOR_R8_D8 (r,operand) ->
             (r8 r).Update (alu.Xor8 operand)
-            PC.Value <- nextInstruction
         | XOR_R8_AR16 (r,ar) ->
             (r8 r).Update (alu.Xor8 <| mmu.Read8 (r16 ar).Value)
-            PC.Value <- nextInstruction
         (*
             Set/Clear/Test bits
         *)
         | SET_R8 (n,r) ->
             (r8 r).Update (setBit n)
-            PC.Value <- nextInstruction
         | SET_AR16 (n,r) ->
             mmu.Update8 (r16 r).Value (setBit n)
-            PC.Value <- nextInstruction
         | RES_R8 (n,r) ->
             (r8 r).Update (clearBit n) 
-            PC.Value <- nextInstruction
         | RES_AR16 (n,r) ->
             mmu.Update8 (r16 r).Value (clearBit n)
-            PC.Value <- nextInstruction
         | BIT_R8 (n,r) ->
             F.Z <- bitStateOf n (r8 r).Value |> bitStateInvert
             F.NH <- (CLEAR, SET)
-            PC.Value <- nextInstruction
         | BIT_AR16 (n,r) ->
             F.Z <- bitStateOf n (mmu.Read8 (r16 r).Value) |> bitStateInvert
             F.NH <- (CLEAR, SET)
-            PC.Value <- nextInstruction
         (*
             Jumps
         *)
@@ -332,60 +277,54 @@ type CPU (mmu, timerInterrupt: TimerInterrupt, clock: MutableClock) as this =
         | JP_AR16 (r) ->
             PC.Value <- mmu.Read16 (r16 r).Value
         | JP_F_A16 (f,address) ->
-            PC.Value <-
-                match (F.FlagFromName f) with
-                | SET ->
-                    longCycle <- true
-                    address
-                | CLEAR ->
-                    nextInstruction
+            match (F.FlagFromName f) with
+            | SET ->
+                longCycle <- true
+                PC.Value <- address
+            | CLEAR ->
+                ()
         | JP_NF_A16 (f,address) ->
-            PC.Value <-
-                match (F.FlagFromName f) with
-                | SET ->
-                    nextInstruction
-                | CLEAR ->
-                    longCycle <- true
-                    address
+            match (F.FlagFromName f) with
+            | SET ->
+                ()
+            | CLEAR ->
+                longCycle <- true
+                PC.Value <- address
         | JR_A8 (offset) ->
-            PC.Value <- (int16 nextInstruction) + (int16 offset) |> uint16 
+            PC.Value <- (int16 PC.Value) + (int16 offset) |> uint16 
         | JR_F_A8 (f, offset) ->
-            PC.Value <-
-                match (F.FlagFromName f) with
-                | SET ->
-                    longCycle <- true
-                    (int16 nextInstruction) + (int16 offset) |> uint16
-                | CLEAR ->
-                    nextInstruction
+            match (F.FlagFromName f) with
+            | SET ->
+                longCycle <- true
+                PC.Value <- (int16 PC.Value) + (int16 offset) |> uint16
+            | CLEAR ->
+                ()
         | JR_NF_A8 (f, offset) ->
-            PC.Value <-
-                match (F.FlagFromName f) with
-                | CLEAR ->
-                    longCycle <- true
-                    (int16 nextInstruction) + (int16 offset) |> uint16
-                | SET ->
-                    nextInstruction
+            match (F.FlagFromName f) with
+            | CLEAR ->
+                longCycle <- true
+                PC.Value <- (int16 PC.Value) + (int16 offset) |> uint16
+            | SET ->
+                ()
         | CALL_A16 (address) ->
-            push16 nextInstruction
+            push16 PC.Value
             PC.Value <- address
         | CALL_F_A16 (flag, address) ->
-            PC.Value <-
-                match (F.FlagFromName flag) with
-                | SET ->
-                    push16 nextInstruction
-                    longCycle <- true
-                    address
-                | CLEAR ->
-                    nextInstruction
-        | CALL_NF_A16 (flag, address) ->
-            PC.Value <-
-                match (F.FlagFromName flag) with
-                | SET ->
-                    nextInstruction
-                | CLEAR ->
-                    push16 nextInstruction
-                    longCycle <- true
-                    address
+            match (F.FlagFromName flag) with
+            | SET ->
+                push16 PC.Value
+                longCycle <- true
+                PC.Value <- address
+            | CLEAR ->
+                ()
+        | CALL_NF_A16 (flag, address) ->  
+            match (F.FlagFromName flag) with
+            | SET ->
+                ()
+            | CLEAR ->
+                push16 PC.Value
+                longCycle <- true
+                PC.Value <- address
         | RET ->
             PC.Value <- pop16 ()
         | RETI ->
@@ -397,11 +336,11 @@ type CPU (mmu, timerInterrupt: TimerInterrupt, clock: MutableClock) as this =
                 PC.Value <- pop16 ()
                 longCycle <- true
             | CLEAR ->
-                PC.Value <- nextInstruction
+                ()
         | RET_NF (flag) ->
             match (F.FlagFromName flag) with
             | SET ->
-                PC.Value <- nextInstruction
+                ()
             | CLEAR ->
                 PC.Value <- pop16 ()
                 longCycle <- true
@@ -411,20 +350,16 @@ type CPU (mmu, timerInterrupt: TimerInterrupt, clock: MutableClock) as this =
         *)
         | SCF ->
             F.NHC <- (CLEAR, CLEAR, SET)
-            PC.Value <- nextInstruction
         | CCF ->
             F.NHC <- (CLEAR, CLEAR, bitStateInvert F.C)
-            PC.Value <- nextInstruction
         | NOP ->
-            PC.Value <- nextInstruction
+            ()
         | STOP ->
             () // Do nooooothing
         | EI ->
             MasterIE.Set
-            PC.Value <- nextInstruction
         | DI ->
             MasterIE.Clear
-            PC.Value <- nextInstruction
         | DAA_R8 (r) ->
             let value = (r8 r).Value
             
@@ -438,13 +373,10 @@ type CPU (mmu, timerInterrupt: TimerInterrupt, clock: MutableClock) as this =
 
             (r8 r).Value <- result
 
-            PC.Value <- nextInstruction
         | FGBC_PRINT_R8 (r) ->
             printfn "%d" (r8 r).Value
-            PC.Value <- nextInstruction
         | FGBC_PRINTA_R8 (r) ->
             printf "%c" <| char (r8 r).Value 
-            PC.Value <- nextInstruction
         | _ -> raise (System.Exception(sprintf "opcode <%O> not implemented" instruction))
 
         // Update clock

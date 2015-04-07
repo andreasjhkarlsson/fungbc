c7653c447d8919834812b80d0655d291754a8191
Andreas Karlsson <andreas.karlsson3@gmail.com>
2015-04-07 20:18:43 +0200

    Add gpu info to debugger

--- Debugger.fs
+++ Debugger.fs
@@ -152,6 +152,11 @@ type Debugger(cpu: CPU,gpu: GPU, mmu: MMU,interrupts: InterruptManager, systemCl
             "remove breakpoint (d), " +
             "print interrupt status (q), "+
             "find symbol (y), " +
+            "print gpu info (g), "+
+            "print tile map0 (m0), "+
+            "print tile map1 (m1), "+
+            "print tile data 0 (t0), "+
+            "print tile data 1 (t1), "+
             "print symbols (u), " +
             "print registers (r), " +
             "print stack (z), " +
@@ -226,6 +231,70 @@ type Debugger(cpu: CPU,gpu: GPU, mmu: MMU,interrupts: InterruptManager, systemCl
                 | None ->
                     printError "Invalid command"
                 interactive ()
+            | "g" ->
+                let lcdc = gpu.Registers.LCDC
+                let lcds = gpu.Registers.LCDS
+
+                let mapToDescription = function |Map0 -> "map0 (0x9800 - 0x9BFF)" |Map1 -> "map1 (0x9C00 - 0x9FFF)"
+                
+                let tileDataSelectToDescription = function |Tiles0 -> "tiles0 (0x8800 - 0x97FF)" |Tiles1 -> "tiles1 (0x8000 - 0x8FFF)"
+
+                printResult "GPU Status\n"
+                printResult "LCDC (0xFF40):"
+                printResult <| sprintf "\tRaw value: 0x%02X" lcdc.Value
+                printResult <| sprintf "\tDisplay enable (7): %b" lcdc.DisplayEnable
+                printResult <| sprintf "\tWindow tile map (6): %s" (mapToDescription lcdc.WindowTileMapSelect)
+                printResult <| sprintf "\tWindow enable (5): %b" lcdc.WindowEnabled
+                printResult <| sprintf "\tBG & Window tile data select (4): %s" (tileDataSelectToDescription lcdc.BGAndWindowTileDataSelect)
+                printResult <| sprintf "\tBG Tile map (3): %s" (mapToDescription lcdc.BGTilemapSelect)
+                printResult <| sprintf "\tSprite size (2):  %A" lcdc.SpriteSize
+                printResult <| sprintf "\tSprite enable (1): %b" lcdc.SpriteEnable
+                printResult <| sprintf "\tBG enable (0): %b" lcdc.BGDisplay
+                printResult ""
+
+                printResult "LCDS (0xFF41):"
+                printResult <| sprintf "\tRaw value: 0x%02X" lcds.Value
+                printResult <| sprintf "\tLYC=LY interrupt enable (6): %b" lcds.LYCLYCoincidenceInterrupt
+                printResult <| sprintf "\tOAM interrupt enable (5): %b" lcds.OAMInterrupt
+                printResult <| sprintf "\tVBlank interupt enable (4): %b" lcds.VBlankInterrupt
+                printResult <| sprintf "\tHBlank interupt enable (3): %b" lcds.HBlankInterrupt
+                printResult <| sprintf "\tLYC = LY (2):  %b" (if lcds.Coincidence = LYC_E_LY then true else false)
+                printResult <| sprintf "\tMode (1-0): %s" (match lcds.Mode with
+                                                            |Mode.VBlank -> "VBlank"
+                                                            |Mode.HBlank -> "HBlank"
+                                                            |Mode.LCDDriverDataTransfer -> "LCD Data transfer"
+                                                            |Mode.SearchingOAMRAM -> "Saerching OAM-RAM")
+                printResult ""
+
+                printResult <| sprintf "SCY (0xFF42): %d" gpu.Registers.SCY.Value
+                printResult <| sprintf "SCX (0xFF43): %d" gpu.Registers.SCX.Value
+
+                printResult <| sprintf "LY (0xFF44): %d" gpu.Registers.LY.Value
+                printResult <| sprintf "LYC (0xFF45): %d" gpu.Registers.LYC.Value
+
+                printResult <| sprintf "BGP (0xFF47): 0x%02X" gpu.Registers.BGP.Value
+
+                interactive ()
+            | "m0" ->
+                setResultFormatting ()
+                this.PrintMemory 0x9800 0x9BFF
+                clearFormatting ()
+                interactive ()
+            | "m1" ->
+                setResultFormatting ()
+                this.PrintMemory 0x9C00 0x9FFF
+                clearFormatting ()
+                interactive ()     
+            | "t0" ->
+                setResultFormatting ()
+                this.PrintMemory 0x8800 0x97FF
+                clearFormatting ()
+                interactive ()
+            | "t1" ->
+                setResultFormatting ()
+                this.PrintMemory 0x8000 0x8FFF
+                clearFormatting ()
+                interactive ()            
             | "y" ->
                 match parameter with
                 | AddressParameter address ->

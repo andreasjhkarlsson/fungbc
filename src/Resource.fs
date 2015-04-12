45d612f7595994559fea082114d5bec988f5b3c1
Andreas Karlsson <andreas.karlsson3@gmail.com>
2015-04-12 13:20:35 +0200

    Add file forgotten in last commit

---
+++ src/Resource.fs
@@ -0,0 +1,18 @@
+﻿module Resource
+
+open System.Drawing
+open System.Reflection
+open System.Resources
+open System.Windows.Forms
+
+
+let executingAssembly = Assembly.GetExecutingAssembly()
+let resources = new ResourceManager("Resources", executingAssembly)
+
+let stringResource = resources.GetString
+
+let title = stringResource "title"
+
+let about = stringResource "about"
+
+let icon = resources.GetObject("icon") :?> Icon
